4cf7faab1a34c61910797adfbf804dbad718e85e
mmayye <mmayye@myelitebook01>
2018-12-18 13:29:26 +0800

    1

--- ume-cloud-gateway/src/main/java/org/umeframework/cloud/Application.java
+++ ume-cloud-gateway/src/main/java/org/umeframework/cloud/Application.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
+import org.umeframework.dora.appconfig.AutoConfigurationBasic;
+import org.umeframework.dora.appconfig.AutoConfigurationDao;
 import org.umeframework.dora.message.MessageProperties;
 
 /**
@@ -20,7 +21,7 @@ import org.umeframework.dora.message.MessageProperties;
  */
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration.class, org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class, org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class })
-@Import({ org.umeframework.dora.appconfig.AutoConfigurationBasic.class, })
+@Import({ AutoConfigurationBasic.class, AutoConfigurationDao.class })
 @ComponentScan(basePackages = "org.umeframework.cloud")
 public class Application {
 
@@ -31,18 +32,25 @@ public class Application {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        try {
+            SpringApplication.run(Application.class, args);
+        } catch (Throwable e) {
+            if (!e.getClass().getName().startsWith("org.springframework.boot.devtools")) {
+                e.printStackTrace();
+            }
+        }
     }
-    
-//    @Bean
-//    public RouteLocator customizeRouter(RouteLocatorBuilder builder) {
-//        StripPrefixGatewayFilterFactory.Config config = new StripPrefixGatewayFilterFactory.Config();
-//        config.setParts(1);
-//        return builder.routes()
-//                .route("host_route", r -> r.path("/a/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8081"))
-//                .route("host_route", r -> r.path("/b/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8082"))
-//                .build();
-//    }
+
+    /**
+     * myRoutes
+     * 
+     * @param builder
+     * @return
+     */
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().build();
+    }
 
     /**
      * Create RestTemplate instance.<br>
@@ -56,14 +64,14 @@ public class Application {
     }
 
     /**
-     * messageProperties
+     * routeMapping
      * 
      * @return
      * @throws Exception
      */
-    @Bean(name = "appMapping")
-    public MessageProperties appMapping() throws Exception {
-        return new org.umeframework.dora.message.impl.MessagePropertiesImpl("appMapping");
+    @Bean(name = "routeMapping")
+    public MessageProperties routeMapping() throws Exception {
+        return new org.umeframework.dora.message.impl.MessagePropertiesImpl("routeMapping");
     }
 
 }
